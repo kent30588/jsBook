fb4e53543d495940d8a236605b2c06910d8e8fd3
Alexandr <ak4raulov@yandex.ru>
2021-08-18 17:26:50 +0300

    логические операторы  циклы решения дз

--- src/js/main.js
+++ src/js/main.js
@@ -76,3 +76,135 @@
 //for (let i = 200; i <= 250; i ++){
 //	console.log(i);
 //}
+
+// Операторы
+
+//console.log(typeof(+'1' + +'3')); // получим  4
+
+//инкрименты
+//let i = 3;
+//i++;
+//console.log(i); // получим значение 4
+
+//let i = 3;
+//console.log(++i); // получим значение 4
+
+//декремент
+
+//let i = 3;
+//i--;
+//console.log(i); // получим значение 2
+
+//let i = 3;
+//console.log(--i); // получим значение 2
+
+//Возведение в степень
+//let a = 2;
+//let b = 3;
+//console.log(2 ** 3); // получим значение 8
+//console.log(3 ** 2); // получим значение 9
+
+//Остаток от деления
+//let a = 11;
+//let b = 2;
+//console.log(a % b);// получим 1
+
+//Логические операторы
+
+//  Оператор ||
+
+//let a = true;
+//let b = false;
+//if (a || b) {
+//	alert('Я буду появляться т.к. одно из значение true');
+//} else {
+//	alert('Появлюсь я');
+//}
+
+
+//let c = false;
+//let d = false;
+//if (c || d) {
+//	alert('Я буду появляться т.к. одно из значение true');
+//} else {
+//		alert('Появлюсь я');
+//}
+
+//let useAge = 17;
+//let counrMsgs = 499;
+
+//if ((useAge >= 18) || (counrMsgs >= 500)) {
+//	alert('Вам разрешено прикреплять фото');// выведет сообщение, если хоть один операнд будет true
+//} else {
+//	alert('Вам запрещено прикреплять фото');// выведет сообщение, если оба false
+//}
+
+// Оператор &&
+
+//console.log(false && false);//false
+//console.log(true && false);//false
+//console.log(false && true);//false
+//console.log(true && true);// true
+
+
+
+//let useAge = 21;
+//let counrMsgs = 499;
+
+//if ((useAge >= 18) && (counrMsgs >= 500)) {
+//	alert('Вам разрешено прикреплять фото');// выведет сообщение, если оба операнда true
+//} else {
+//	alert('Вам запрещено прикреплять фото');// выведет сообщение, если оба или один false
+//}
+
+// Оператор ! 
+
+//let a = !true;
+//console.log(a);// вернет false
+//let b = !false;
+//console.log(b);// вернет true
+//let c = !'string';
+//console.log(c);// вернет false
+//let d = !0;
+//console.log(d);// вернет true
+//let e = !8;
+//console.log(e);// вернет false
+
+// !!
+
+//let str = 5;
+//let a = !!str;
+//console.log(a); // перевели переменную str в boolean / true
+
+/* домашнее задание
+Пользователю с помощью функции prompt предлагается ввести возраст
+
+Если возраст больше 18 лет, то с помощью еще одгного prompt, нужно запросить имя пользователя
+все эти данные нужно сохранить в переменных.
+
+Как только ты получишь от пользователя все необходимые данные,
+ты должен сделать следующие проверки: ЕСЛИ пользователю 25 лет и его имя John,
+ты должен с помощью функции alert() вывести фразу "Welcome, John". 
+В противном случае ты должен вывести фразу "Who are you? I don`t know you"
+*/
+
+//Мое решение. 
+
+/*
+let age = +prompt('Введите ваш возраст', '');
+let userName = null;
+
+//console.log(typeof(age));
+if (age >= 18) {
+	userName = prompt('Введите ваше имя');
+} else {
+	alert('Исполнится 18 лет, тода заходи');
+}
+
+if ((age === 25) && (userName === 'John')) {
+	alert('Welcome, John');
+} else {
+	alert('Who are you? I don`t know ');
+}
+*/
+
